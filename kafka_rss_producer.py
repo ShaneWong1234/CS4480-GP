f4379140adac2a6dda6d8768b63aebf3371da486
ben-edde <ben.0xedde@gmail.com>
2021-11-14 19:10:22 +0800

    updated rss ;

--- kafka_rss_producer.py
+++ kafka_rss_producer.py
@@ -1,11 +1,21 @@
 from kafka import KafkaProducer
 import time
+import datetime
 import pandas as pd
 import feedparser
-newsfeed = feedparser.parse("https://feeds.bbci.co.uk/news/rss.xml")
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
+feed_url="https://feeds.bbci.co.uk/news/rss.xml"
+last_updated_time = None
 
-for each in newsfeed.entries:    
-    msg=f"{each.title}"
-    print(msg)
-    producer.send('news-raw',value=bytes(msg,encoding="UTF-8"))
+while True:
+    newsfeed = feedparser.parse(feed_url)
+    current_updated_time=datetime.datetime.fromtimestamp(time.mktime(newsfeed.feed.updated_parsed))
+    if current_updated_time == last_updated_time:
+        time.sleep(30)
+        continue
+    last_updated_time=current_updated_time
+
+    for each in newsfeed.entries:    
+        msg=f"{each.title}"
+        print(msg)
+        producer.send('news-raw',value=bytes(msg,encoding="UTF-8"))
